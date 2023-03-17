0f5e546497b1164799aeb563ed66b3eb08991131
eldaroo <darioegea@gmail.com>
2023-03-17 11:28:18 -0300

    add TTL logic, and background task to remove the carts when are expired

--- src/main/java/com/xcale/challengeaccepted/model/Cart.java
+++ src/main/java/com/xcale/challengeaccepted/model/Cart.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class Cart {
     private final Map<Integer, Product> products = new HashMap<>();
+    private final long creationTime = System.currentTimeMillis();
+    private static final long TTL = 10 * 60 * 10000; // 10 minutes in milliseconds
     Integer uniqueId = 1;
 
     Integer getUniqueId() {
@@ -11,5 +13,9 @@ public class Cart {
     }
     public void addProduct(Product product) {
         products.put(getUniqueId(), product);
+    }
+
+    public boolean isExpired() {
+        return System.currentTimeMillis() - creationTime > TTL;
     }
 }
--- src/main/java/com/xcale/challengeaccepted/service/CartServiceImpl.java
+++ src/main/java/com/xcale/challengeaccepted/service/CartServiceImpl.java
@@ -1,15 +1,19 @@
 package com.xcale.challengeaccepted.service;
 
+import com.xcale.challengeaccepted.exception.InvalidCartIdException;
 import com.xcale.challengeaccepted.model.Cart;
 import com.xcale.challengeaccepted.model.Product;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class CartServiceImpl implements CartService {
 
+    private final int CLEAN_UP_EXPIRES_BG_TIME = 60000; // 10 minutes
     Map<Integer, Cart> carts = new HashMap<>();
     Integer uniqueId = 1;
 
@@ -24,24 +28,35 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
-    public void addProduct(Integer cartId, Product product) throws Exception {
+    public void addProduct(Integer cartId, Product product) throws InvalidCartIdException {
         Cart cart = carts.get(cartId);
         if (cart == null) {
-            throw new Exception("Cart not found: " + cartId);
+            throw new InvalidCartIdException("Invalid cart ID: " + cartId);
         }
         cart.addProduct(product);
     }
 
     @Override
-    public Cart getCartById(Integer cartId) {
-        return carts.get(cartId);
+    public Cart getCartById(Integer cartId) throws InvalidCartIdException{
+        Cart cart = carts.get(cartId);
+        if (cart == null) {
+            throw new InvalidCartIdException("Invalid cart ID: " + cartId);
+        }
+        return cart;
     }
 
     @Override
-    public void removeCart(Integer cartId) throws Exception {
+    public void removeCart(Integer cartId) throws InvalidCartIdException {
         if (carts.remove(cartId) == null) {
-            throw new Exception("Cart not found: " + cartId);
+            throw new InvalidCartIdException("Invalid cart ID: " + cartId);
         }
     }
 
+    @Scheduled(fixedRate = CLEAN_UP_EXPIRES_BG_TIME)
+    public void cleanUpExpiredCarts() {
+        carts = carts.entrySet()
+                .stream()
+                .filter(entry -> !entry.getValue().isExpired())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 }
