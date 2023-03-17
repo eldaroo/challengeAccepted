cd0dd3ad6ddba6c3a4365527881b52001abc51cc
eldaroo <darioegea@gmail.com>
2023-03-17 10:45:07 -0300

    =initializing challengeAccepted spring app

---
+++ src/test/java/com/xcale/challengeaccepted/ChallengeAcceptedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcale.challengeaccepted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAcceptedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
